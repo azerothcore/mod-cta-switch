26a5ad3369b52c38963dfa9eb33ac64ec27388ad
Exitare <business.exitare@icloud.com>
2025-01-13 20:06:03 -0800

    Fix mod to work with PR #21151

--- src/mod_cta_switch_script.cpp
+++ src/mod_cta_switch_script.cpp
@@ -158,7 +158,7 @@ public:
                     GameEventData const& eventData = events[activeEvent];
 
                     sGameEventMgr->StopEvent(activeEvent, true);
-                    LOG_INFO("module", "[CTA-Switch]: Stopping {} ({})", eventData.description, activeEvent);
+                    LOG_INFO("module", "[CTA-Switch]: Stopping {} ({})", eventData.Description, activeEvent);
                 }
             }
         }
